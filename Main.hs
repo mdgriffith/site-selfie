392fc8a9ac21065bafc002bd04cad450efca5341
Matthew Griffith <mdg.griffith@gmail.com>
2015-06-17 20:03:39 -0400

    Add Resolutions

--- Main.hs
+++ Main.hs
@@ -4,31 +4,32 @@ import System.Directory (createDirectoryIfMissing)
 import System.Locale (defaultTimeLocale)
 import Data.Time.Format (formatTime)
 import Data.Time.Clock (getCurrentTime, UTCTime)
+import Data.Word
 
 import qualified Data.ByteString.Lazy as L
 
 import Control.Applicative
 import Control.Monad.IO.Class
+import Control.Concurrent ( threadDelay )
 
 import Test.WebDriver
 
 
-data Base = Base { name::String, url::String, config::WDConfig }
 
-newtype Branch = Branch String
-               deriving (Show, Ord, Eq)
+data Base = Base { name::String, url::String }
 
-getBranch :: Branch -> String
-getBranch (Branch t) = t
+type Branch = String
+
+type Resolution = (Word, Word)
 
 toURL :: Base -> Branch -> String
-toURL base (Branch burl) = (url base) ++ burl
+toURL base branch = (url base) ++ branch
 
-formatPath :: String -> Base -> Branch -> UTCTime -> FilePath
-formatPath root base branch date = root 
+formatPath :: String -> Base -> Branch -> UTCTime -> Resolution -> FilePath
+formatPath root base branch date (w, h) = root 
                                </> (slugifyTime date)
-                               </> (name base) 
-                               </> (getBranch branch)
+                               </> (name base) ++ "_" ++ show w ++ "x" ++ show h
+                               </> branch
                                </> "index.png"
 
 -- formatPNG :: Base -> UTCTime -> FilePath
@@ -38,28 +39,40 @@ slugifyTime :: UTCTime -> String
 slugifyTime utc = formatTime defaultTimeLocale "%F_%H-%M-%S" utc
 
 bases :: [Base]
-bases =  [Base {name="Dev",  url="http://localhost:8000/",          config=defaultConfig},
-          Base {name="Live", url="http://mechanical-elephant.com/", config=defaultConfig}]
+bases =  [Base { name="Dev",  
+                 url="http://localhost:8000/"
+               },
+          Base { name="Live", 
+                 url="http://mechanical-elephant.com/"
+               }
+          ]
+
+resolutions :: [Resolution]
+resolutions = [(1366, 768), (320, 320)]
 
 branches :: [Branch]
-branches = fmap Branch ["", "archive", "about"]
+branches = ["", "archive", "about"]
 
 main :: IO ()
 main = do 
           time <- getCurrentTime
           _ <- sequence $ runScreenshots time
           return ()
-    where runScreenshots time = saveScreenshot "siteshots" time <$> bases <*> branches
+    where runScreenshots time = saveScreenshot "siteshots" time defaultConfig <$> bases <*> branches <*> resolutions
 
-saveScreenshot :: String -> UTCTime -> Base -> Branch -> IO ()
-saveScreenshot root utc base branch = do
-                                        runSession (config base) $ do
-                                            openPage pageUrl
-                                            ss <- screenshot
-                                            liftIO $ createDirectoryIfMissing True (takeDirectory filepath)
-                                            liftIO $ L.writeFile filepath ss
-                                where pageUrl = toURL base branch
-                                      filepath = formatPath root base branch utc
+saveScreenshot :: String -> UTCTime -> WDConfig -> Base -> Branch -> Resolution -> IO ()
+saveScreenshot root utc config base branch res = do
+                runSession config $ do
+                    openPage pageUrl
+                    setWindowSize res
+                    liftIO $ threadDelay 750
+                    ss <- screenshot
+                    window <- getCurrentWindow
+                    closeWindow window
+                    liftIO $ createDirectoryIfMissing True (takeDirectory filepath)
+                    liftIO $ L.writeFile filepath ss
+        where pageUrl = toURL base branch
+              filepath = formatPath root base branch utc res
 
 
 
